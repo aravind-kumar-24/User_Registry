a70efbd7e6aaefa85bd1a584950fb82c2407e96d
aravind-kumar-24 <aravindmpkas@gmail.com>
2025-10-25 22:19:17 +0530

    Login page and Registration page directory setup

---
+++ forms/login/login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Registry</title>
+    <link rel="icon" type="image/x-icon" href="../../assets/images/user_registry_favicon.png"/>
+</head>
+<body>
+    <div>
+        Login page
+    </div>
+</body>
+</html>
