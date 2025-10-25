08e1d0dd206158ecf902af4cb11bd38ffa923016
aravind-kumar-24 <aravindmpkas@gmail.com>
2025-10-25 22:00:43 +0530

    Initial Landing Page design has been completed

--- index.php
+++ index.php
@@ -4,7 +4,24 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>User Registry</title>
+        <link rel="icon" type="image/x-icon" href="./assets/images/user_registry_favicon.png"/>
+        <link rel="stylesheet" href="./assets/css/header.css"/>
+        <link rel="stylesheet" href="./index.css"/>
     </head>
-    <body>
+    <body style="background-color: #FAF8ED;">
+        <?php require_once "./includes/header.inc.php" ?>
+        <div class="container">
+            <div class="container-left">
+                <div class="container-heading">
+                    Welcome to <br/>User Registry!
+                </div>
+                <div class="container-sub-heading">
+                    Easily manage your user profiles <br/> and accounts.
+                </div>
+            </div>
+            <div class="container-right">
+                <img height="500px" class="home_page_image" src="./assets/images/home_page_image_01.png"/>
+            </div>
+        </div>
     </body>
 </html>
