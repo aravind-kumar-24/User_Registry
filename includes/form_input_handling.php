82cbf55a7570991bb2f2daaff2394e3507a4dc4a
aravind-kumar-24 <aravindmpkas@gmail.com>
2025-10-29 22:48:29 +0530

    Registration functionality has been completed

--- includes/form_input_handling.php
+++ includes/form_input_handling.php
@@ -8,4 +8,38 @@
         return $data;
     }
 
+    function email_id_existing_check($connection, $email_id){
+        $email_existing_check_query = "
+            SELECT * FROM users
+            WHERE email_id = :email_id;
+        ";
+
+        $email_existing_check_statement = $connection->prepare($email_existing_check_query);
+
+        $email_existing_check_statement->bindParam(":email_id", $email_id);
+
+        $email_existing_check_statement->execute();
+
+        $results = $email_existing_check_statement->fetchAll(PDO::FETCH_ASSOC);
+
+        return $results;
+    }
+
+    function mobile_existing_check($connection, $contact_number){
+        $mobile_existing_check_query = "
+            SELECT * FROM users
+            WHERE mobile_number = :contact_number;
+        ";
+
+        $mobile_existing_check_statement = $connection->prepare($mobile_existing_check_query);
+
+        $mobile_existing_check_statement->bindParam(":contact_number", $contact_number);
+
+        $mobile_existing_check_statement->execute();
+
+        $results = $mobile_existing_check_statement->fetchAll(PDO::FETCH_ASSOC);
+
+        return $results;
+    }
+
 ?>
